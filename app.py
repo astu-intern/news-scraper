e76130a3de6328ade6a927b5b187f97c48499c98
astu-intern <astu.intern@gmail.com>
2024-07-10 16:03:48 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ import string
 import datetime
 import csv
 
-
 # File upload
 uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
 
@@ -22,29 +21,36 @@ if uploaded_file is not None:
     df = df.head(1)
     # Display the dataframe
     st.write(df)
-    
+
     # Save the uploaded file to process it
     with open("uploaded_file.csv", "wb") as f:
         f.write(uploaded_file.getbuffer())
-    
+
     st.success("File saved successfully.")
 
-    keywords_file = 'trial_keywords.csv'
+    keywords_file = 'keywordsfile.csv'
     output_filename = 'webscraper_output'
 
-    df['Tertiary Keywords'] = df ['CITY']
-    df = df[['Tertiary Keywords']]
+    df['Tertiary Keywords'] = df['CITY']
+    df = df[['Tertiary Keywords','START_DATE','END_DATE']]
     
+
     headlines_df = webscraper(df, keywords_file, output_filename)
-    st.write(headlines_df)
-    
+
+
+
+    transport_tokenizer = "augmented_models/transport/tokenizer"
+    transport_model = "augmented_models/transport/model"
+    industrial_tokenizer = "augmented_models/transport/tokenizer"
+    industrial_model = "augmented_models/transport/model"
+    manmade_tokenizer = "augmented_models/transport/tokenizer"
+    manmade_model = "augmented_models/transport/model"
+    natural_tokenizer = "augmented_models/transport/tokenizer"
+    natural_model = "augmented_models/transport/model"
+
+
 
     unpredicted_data = category_predictor(headlines_df)
-    predicted_data = status_predictor(unpredicted_data)
-    results = predictions_compiler(predicted_data)
-
-    for i in results:
-        st.write(i)
-
-    for k in predicted_data:
-        st.write(k)
+    predicted_data = status_predictor(unpredicted_data[3],transport_model, transport_tokenizer)
+    st.header('Predictions:')
+    st.write(predicted_data)
