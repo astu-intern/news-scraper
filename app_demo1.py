8ea34626fa037051018a49d89fb3337cef3d6719
astu-intern <astu.intern@gmail.com>
2025-04-04 16:08:51 +0530

    Update app_demo1.py

--- app_demo1.py
+++ app_demo1.py
@@ -37,7 +37,6 @@ if uploaded_file is not None:
     df['PROMISED_DATE']=pd.to_datetime(df['PROMISED_DATE'])
     df['START_DATE']=df['PROMISED_DATE'].apply(lambda x: x - relativedelta(months=3))
     df['END_DATE']=df['PROMISED_DATE']
-    st.write(df['Tertiary Keywords'],df['START_DATE'],df['END_DATE'])
     df = df[['Tertiary Keywords','START_DATE','END_DATE']].copy()
     
 
