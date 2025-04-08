4185379a824f81ab7c5937ef7f11b4adbfa122ca
astu-intern <astu.intern@gmail.com>
2025-04-08 16:46:47 +0530

    Update functions_demo1.py

--- functions_demo1.py
+++ functions_demo1.py
@@ -389,6 +389,7 @@ def webscraper(cities_df, keywords_file = 'trial_keywords.csv', output_filename=
         st.stop()
     else:
         st.write(df)
+        st.write("Test")
         return df
 
 
