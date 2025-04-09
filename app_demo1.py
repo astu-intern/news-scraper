11bbf99270fd89e8ae97dd21c74ea7bef73c088c
astu-intern <astu.intern@gmail.com>
2025-04-09 12:25:24 +0530

    Update app_demo1.py

--- app_demo1.py
+++ app_demo1.py
@@ -34,9 +34,8 @@ if uploaded_file is not None:
     output_filename = 'webscraper_output'
 
     df['Tertiary Keywords'] = df['CITY']
-    df['PROMISED_DATE']=pd.to_datetime(df['PROMISED_DATE'])
-    df['START_DATE']=df['PROMISED_DATE'].apply(lambda x: x - relativedelta(months=6))
-    df['END_DATE']=df['PROMISED_DATE']
+    df['START_DATE']=pd.to_datetime(df['START_DATE'])
+    df['END_DATE']=pd.to_datetime(df['END_DATE'])
     df['Vendor']=df['VENDOR']
     df = df[['Vendor','Tertiary Keywords','START_DATE','END_DATE']].copy()
     
